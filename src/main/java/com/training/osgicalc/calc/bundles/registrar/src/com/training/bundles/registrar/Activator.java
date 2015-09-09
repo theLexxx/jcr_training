95ea5b62a8b4f4e24cbe4c5e3f7cefc64e2caef4
Aliaksei Kuk <Aliaksei_Kuk@EPBYGOMW0062.gomel.epam.com>
2015-09-09 17:41:17 +0300

    replace service listener to bundle listener

--- src/main/java/com/training/osgicalc/calc/bundles/registrar/src/com/training/bundles/registrar/Activator.java
+++ src/main/java/com/training/osgicalc/calc/bundles/registrar/src/com/training/bundles/registrar/Activator.java
@@ -3,21 +3,22 @@ package com.training.bundles.registrar;
 import com.training.bundles.registrar.listeners.OperationRegistrar;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleListener;
 
 public class Activator implements BundleActivator {
-    private OperationRegistrar operationRegistrar;
+    private BundleListener operationRegistrar;
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         System.out.println("Starting to listen and register \"Operation\" bundles");
 
         operationRegistrar = new OperationRegistrar();
-        bundleContext.addServiceListener(operationRegistrar);
+        bundleContext.addBundleListener(operationRegistrar);
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-        bundleContext.addServiceListener(operationRegistrar);
+        bundleContext.removeBundleListener(operationRegistrar);
         System.out.println("Stopped listening and registering \"Operation\" bundles");
     }
 }
--- src/main/java/com/training/osgicalc/calc/bundles/registrar/src/com/training/bundles/registrar/listeners/OperationRegistrar.java
+++ src/main/java/com/training/osgicalc/calc/bundles/registrar/src/com/training/bundles/registrar/listeners/OperationRegistrar.java
@@ -1,11 +1,13 @@
 package com.training.bundles.registrar.listeners;
 
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
 
-public class OperationRegistrar implements ServiceListener {
+public class OperationRegistrar implements BundleListener {
+
     @Override
-    public void serviceChanged(ServiceEvent serviceEvent) {
-
+    public void bundleChanged(BundleEvent bundleEvent) {
+        System.out.println("Q!" + bundleEvent.toString());
+        System.out.println("1\t" + bundleEvent.getBundle().getHeaders().toString());
     }
 }
